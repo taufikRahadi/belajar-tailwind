a76d34ec7d48c205b04b198a69e3cf673f73a82c
taufik <taufik@pop-os.localdomain>
2020-08-16 01:28:15 +0700

    belajar tailwind

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const burger = document.querySelector('#burger')
+const menu = document.querySelector('#menu')
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden')
+    } else {
+        menu.classList.add('hidden')
+    }
+})
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+
+  // Optional for webpack-dev-server
+  devServer: {
+    watchContentBase: true,
+    contentBase: path.resolve(__dirname, "dist"),
+    open: true,
+  },
+}
